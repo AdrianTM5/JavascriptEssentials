abfb8c14f45defef6f025f2f38d2e2c5f738fcaf
AdrianTM5 <angel220803@hotmail.com>
2026-01-19 19:17:51 -0600

    followers button

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -9,3 +9,19 @@ function calculateArea() {
 
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+function calculateTotal() {
+
+    g1 = parseFloat(document.getElementById('g1').value);
+    g2 = parseFloat(document.getElementById('g2').value);
+    g3 = parseFloat(document.getElementById('g3').value);
+
+    if(g1 >= 0 && g2 >= 0 && g3 >= 0) {
+        pourchaseTotal = g1 + g2 + g3;
+        document.getElementById('total').innerText = `The total of the pourchase is: ${pourchaseTotal}`;
+    } else {
+        document.getElementById('total').innerText = "Please use a real price";
+    }
+    
+    
+}
